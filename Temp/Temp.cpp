a91622b7cf505480fada9573af51b971f0ce61e2
Tracy <toranome009@gmail.com>
2017-04-24 22:46:00 +0200

    add a window

--- Temp/Temp.cpp
+++ Temp/Temp.cpp
@@ -2,10 +2,51 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <GLFW/glfw3.h>
 
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
 
 int main()
 {
+	if (!glfwInit())
+	{
+		std::cout << "FAIL: glfw not initialised";
+		return -1;
+	}
+
+	glfwSetErrorCallback(error_callback);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "WHOOHOO!", NULL, NULL);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	if (!window)
+	{
+		std::cout << "FAIL: window not created";
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
     return 0;
 }
 
