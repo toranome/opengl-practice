75fb891f9fe2c53cfdf8ab1786a77909896f618d
Tracy <toranome009@gmail.com>
2017-04-27 22:55:56 +0200

    not working

--- Temp/Temp.cpp
+++ Temp/Temp.cpp
@@ -4,6 +4,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void setVertexBuffer(GLuint* bufferPtr, const float* vertices);
+void error_callback(int error, const char* description);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
@@ -17,30 +21,26 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
-// Shader sources
-const GLchar* vertexSource = R"glsl(
-    #version 150 core
-    in vec2 position;
-    in vec3 color;
-    out vec3 Color;
-    void main()
-    {
-        Color = color;
-        gl_Position = vec4(position, 0.0, 1.0);
-    }
-)glsl";
-const GLchar* fragmentSource = R"glsl(
-    #version 150 core
-    in vec3 Color;
-    out vec4 outColor;
-    void main()
-    {
-        outColor = vec4(Color, 1.0);
-    }
-)glsl";
-
-int main()
+void setVertexBuffer(GLuint* bufferPtr, const float* vertices)
 {
+	glGenBuffers(1, bufferPtr);
+	glBindBuffer(GL_ARRAY_BUFFER, *bufferPtr);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+/*
+GLuint setShader(GLenum shaderType, const GLchar* shaderSource, GLint* shaderStatus)
+{
+	GLuint shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &shaderSource, NULL);
+	glCompileShader(shader);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, shaderStatus);
+
+	return shader;
+}
+*/
+
+void runGlLoop(const GLchar* vertexSource, const GLchar* fragmentSource) {
 	if (!glfwInit())
 	{
 		std::cout << "FAIL: glfw not initialised";
@@ -76,24 +76,35 @@ int main()
 	glBindVertexArray(vao);
 
 	GLuint vertexBuffer;
-	glGenBuffers(1, &vertexBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	GLint positiveAttribute;
+	GLuint shaderProgram;
+
+	GLuint fragmentShader;
+	GLint fragmentShaderStatus;
+	GLuint vertexShader;
+	GLint vertexShaderStatus;
+
+	setVertexBuffer(&vertexBuffer, vertices);
+
+	//vertexShader = setShader(GL_VERTEX_SHADER, vertexSource, &vertexShaderStatus);
+	//fragmentShader = setShader(GL_FRAGMENT_SHADER, fragmentSource, &fragmentShaderStatus);
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
 	glCompileShader(vertexShader);
-	GLint vertexShaderStatus;
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderStatus);
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
-	GLint fragmentShaderStatus;
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderStatus);
-	GLuint shaderProgram = glCreateProgram();
+
+	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	GLint positiveAttribute = glGetAttribLocation(shaderProgram, "position");
+
+	positiveAttribute = glGetAttribLocation(shaderProgram, "position");
 	glEnableVertexAttribArray(positiveAttribute);
 	glVertexAttribPointer(positiveAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
@@ -107,7 +118,7 @@ int main()
 
 		glfwGetFramebufferSize(window, &width, &height);
 
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glViewport(0, 0, width, height);
@@ -129,7 +140,32 @@ int main()
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
+}
 
-    return 0;
+// Shader sources
+const GLchar* simpleVertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    in vec3 color;
+    out vec3 Color;
+    void main()
+    {
+        Color = color;
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+const GLchar* simpleFragmentSource = R"glsl(
+    #version 150 core
+    in vec3 Color;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(Color, 1.0);
+    }
+)glsl";
+
+int main()
+{
+	runGlLoop(simpleVertexSource, simpleFragmentSource);
 	exit(EXIT_SUCCESS);
 }
